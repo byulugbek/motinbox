9223010c047b6ddeea2a7eb8525c5d10e3bfb25c
Azamat <by.azamatalimov@gmail.com>
2021-10-21 00:14:39 +0500

    fixes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
         MONGO_URI: 'mongodb+srv://Azamat:ELS3fCj1xVM7hiBr@motionbox.tjf9c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-        URL_BASE: process.env.NODE_ENV === 'production' ? 'https://motionbox.uz' : 'http://localhost:3000',
+        URL_BASE: process.env.NODE_ENV === 'production' ? 'http://localhost:8000' : 'http://localhost:3000',
     }
 }
